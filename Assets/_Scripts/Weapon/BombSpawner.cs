747cddb8123a0c1e25554f392d2b6dfa55fb9964
Yami-Sora <yamisora002@gmail.com>
2025-07-22 23:10:02 +0700

    bomb update

---
+++ Assets/_Scripts/Weapon/BombSpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BombSpawner : Spawner
+{
+
+    private void Reset()
+    {
+        this.prefabName = "BombPrefab";
+        this.spawnPosName = "BombSpawnPos";
+        this.maxObj = 7; 
+    }
+}
